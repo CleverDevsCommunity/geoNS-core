535cb1438a0a880e59a12dd6922acb96d5cb8ce1
Armin Asefi <arminloveslinux@gmail.com>
2024-07-14 00:52:54 +0330

    removes unnecessary *sqlite3_callback from general types

--- src/libs/core/types.h
+++ src/libs/core/types.h
@@ -6,11 +6,5 @@
 typedef unsigned short ushort;
 typedef unsigned char uchar;
 typedef unsigned long ulong;
-typedef int (*sqlite3_callback)(
-   void*,    /* Data provided in the 4th argument of sqlite3_exec() */
-   int,      /* The number of columns in row */
-   char**,   /* An array of strings representing fields in the row */
-   char**    /* An array of strings representing column names */
-);
 
 #endif // !GEONS_TYPE_H
